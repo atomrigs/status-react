f56e7ad73f8e956d83adf8ad9179c097e8e03221
Andrey Shovkoplyas <motor4ik@gmail.com>
2017-03-27 17:57:07 +0300

    fixes after rebase (+1 squashed commit) Squashed commits: [bb4a409] fixes after rebase (+1 squashed commit) Squashed commits: [297b52d] rebase onto ui-refresh

--- src/status_im/profile/edit/screen.cljs
+++ src/status_im/profile/edit/screen.cljs
@@ -8,6 +8,7 @@
             [status-im.components.toolbar-new.actions :as act]
             [status-im.components.text-field.view :refer [text-field]]
             [status-im.i18n :refer [label]]
+            [status-im.profile.screen :refer [colorize-status-hashtags]]
             [status-im.components.confirm-button :refer [confirm-button]]
             [status-im.components.chat-icon.screen :refer [my-profile-icon]]
             [status-im.components.context-menu :refer [context-menu]]
@@ -18,8 +19,9 @@
                                                 text
                                                 touchable-highlight
                                                 text-input]]
-            [status-im.utils.platform :refer [ios?]]
-            [cljs.spec :as s]))
+            [cljs.spec :as s]
+            [clojure.string :as str]
+            [status-im.utils.platform :refer [ios?]]))
 
 (defn edit-my-profile-toolbartoolbar []
   [toolbar {:title   (label :t/edit-profile)
@@ -29,11 +31,10 @@
   [new-profile-name [:get-in [:profile-edit :name]]]
   [view
    [text-field
-    {:wrapper-style     st/profile-name-wrapper
-     :line-color        st/edit-line-color
+    {:line-color        st/edit-line-color
      :focus-line-color  st/profile-focus-line-color
      :focus-line-height st/profile-focus-line-height
-     :label-hidden?     true
+     :label             (label :t/name)
      :input-style       st/profile-name-input
      :on-change-text    #(dispatch [:set-in [:profile-edit :name] %])
      :value             new-profile-name}]])
@@ -44,38 +45,47 @@
 
 (defn edit-profile-bage [contact]
   [view st/edit-profile-bage
-   [view
+   [view st/edit-profile-icon-container
     [context-menu
      [my-profile-icon {:account contact
                        :edit?   true}]
      profile-icon-options
      st/context-menu-custom-styles]]
    [view st/edit-profile-name-container
-    [text {:style st/edit-name-title}
-     (label :t/name)]
     [profile-name-input]]])
 
-(defn edit-profile-status [{:keys [status]}]
+(defn edit-profile-status [{:keys [status edit-status?]}]
   [view st/edit-profile-status
-   [view
-    [text {:style st/edit-status-title} "Status"]
-    [view
-     [text-field
-      {:wrapper-style     st/profile-status-wrapper
-       :line-color        st/edit-line-color
-       :focus-line-color  st/profile-focus-line-color
-       :focus-line-height st/profile-focus-line-height
-       :multiline         true
-       :max-length        140
-       :placeholder       (label :t/profile-no-status)
-       :label-hidden?     true
-       :input-style       st/profile-status-input
-       :on-change-text    #(dispatch [:set-in [:profile-edit :status] %])
-       :value             status}]]]])
+   [scroll-view
+    (if edit-status?
+      [text-input
+       {:auto-focus        edit-status?
+        :multiline         true
+        :max-length        140
+        :placeholder       (label :t/status)
+        :style             st/profile-status-input
+        :on-change-text    #(dispatch [:set-in [:profile-edit :status] %])
+        :on-blur           #(dispatch [:set-in [:profile-edit :edit-status?] false])
+        :blur-on-submit    true
+        :default-value     status}]
+      [touchable-highlight {:on-press #(dispatch [:set-in [:profile-edit :edit-status?] true])}
+       [view
+        (if (str/blank? status)
+          [text {:style st/add-a-status}
+           (label :t/status)]
+          [text {:style st/profile-status-text}
+           (colorize-status-hashtags status)])]])]])
+
+(defn status-prompt [{:keys [status]}]
+  (when (or (nil? status) (str/blank? status))
+    [view st/status-prompt
+     [text {:style st/status-prompt-text}
+      (colorize-status-hashtags "Create a status to help people know about the things you are offering. You can use #hashtags too.")]]))
 
 (defview edit-my-profile []
   [current-account [:get-current-account]
    changed-account [:get :profile-edit]]
+  {:component-will-unmount #(dispatch [:set-in [:profile-edit :edit-status?] false])}
   (let [profile-edit-data-valid? (s/valid? ::v/profile changed-account)
         profile-edit-data-changed? (or (not= (:name current-account) (:name changed-account))
                                        (not= (:status current-account) (:status changed-account))
@@ -84,10 +94,10 @@
                                                   st/profile)
      [status-bar]
      [edit-my-profile-toolbartoolbar]
-     [view {:flex 1}
-      [scroll-view st/edit-my-profile-form
+     [view st/edit-my-profile-form
        [edit-profile-bage changed-account]
-       [edit-profile-status changed-account]]]
+       [edit-profile-status changed-account]
+       [status-prompt changed-account]]
      (when (and profile-edit-data-changed? profile-edit-data-valid?)
        [confirm-button (label :t/save) #(do
                                           (dispatch [:check-status-change (:status changed-account)])
